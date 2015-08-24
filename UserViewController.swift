0ee4e8e0f93b4eda52cba45a363a44c861853554
wilkes <wilkes@tumblr.com>
2015-08-24 17:22:02 -0400

    Initial commit

---
+++ UserViewController.swift
@@ -0,0 +1,101 @@
+//
+//  UserViewController.swift
+//  tipr
+//
+//  Created by Andrew Wilkes on 8/23/15.
+//  Copyright (c) 2015 Andrew Wilkes. All rights reserved.
+//
+
+import UIKit
+
+class UserViewController: UIViewController {
+
+    @IBOutlet weak var lowSlider: UISlider!
+    @IBOutlet weak var mediumSlider: UISlider!
+    @IBOutlet weak var highSlider: UISlider!
+    
+    @IBOutlet weak var lowLabel: UILabel!
+    @IBOutlet weak var mediumLabel: UILabel!
+    @IBOutlet weak var highLabel: UILabel!
+    
+    var tipPercentages = [18, 20, 25]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        println("Loaded UserViewController")
+        
+        var defaults = NSUserDefaults.standardUserDefaults()
+        var low = defaults.integerForKey("lowPercentage")
+        if low != 0 {
+            tipPercentages[0] = low
+        }
+        var medium = defaults.integerForKey("mediumPercentage")
+        if medium != 0 {
+            tipPercentages[1] = medium
+        }
+        var high = defaults.integerForKey("highPercentage")
+        if high != 0 {
+            tipPercentages[2] = high
+        }
+        lowSlider.value = Float(tipPercentages[0])
+        lowSliderChanged(lowSlider)
+        mediumSlider.value = Float(tipPercentages[1])
+        mediumSliderChanged(mediumSlider)
+        highSlider.value = Float(tipPercentages[2])
+        highSliderChanged(highSlider)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func backButtonClicked(sender: AnyObject) {
+        var defaults = NSUserDefaults.standardUserDefaults()
+        defaults.setInteger(Int(lowSlider.value), forKey: "lowPercentage")
+        defaults.setInteger(Int(mediumSlider.value), forKey: "mediumPercentage")
+        defaults.setInteger(Int(highSlider.value), forKey: "highPercentage")
+
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    @IBAction func lowSliderChanged(sender: UISlider) {
+        let newValue = sender.value
+        
+        let toInt = round(newValue)
+        
+        lowLabel.text = "\(Int(toInt))%"
+        
+        sender.value = toInt
+    }
+
+    @IBAction func mediumSliderChanged(sender: UISlider) {
+        let newValue = sender.value
+        
+        let toInt = round(newValue)
+        
+        mediumLabel.text = "\(Int(toInt))%"
+        
+        sender.value = toInt
+
+    }
+    
+    @IBAction func highSliderChanged(sender: UISlider) {
+        let newValue = sender.value
+        
+        let toInt = round(newValue)
+        
+        highLabel.text = "\(Int(toInt))%"
+        
+        sender.value = toInt
+    }
+}
--- tipr/ViewController.swift
+++ tipr/ViewController.swift
@@ -8,11 +8,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
+    @IBOutlet weak var billField:
+        UITextField!
+    @IBOutlet weak var tipLabel:
+        UILabel!
+    @IBOutlet weak var totalLabel:
+        UILabel!
+    @IBOutlet weak var tipControl:
+        UISegmentedControl!
+    
+    var tipPercentages = [18, 20, 25]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tipLabel.text = "$0.00"
+        totalLabel.text = "$0.00"
+        println("Loaded ViewController")
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +35,66 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onEditingChanged(sender: AnyObject) {
+        
+        var percentage = Double(tipPercentages[tipControl.selectedSegmentIndex]) / 100.0
+        
+        var billAmount = NSString(string: billField.text).doubleValue
+        var tip = billAmount * percentage
+        var total = billAmount + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
 
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func onSwipe(sender: AnyObject) {
+        performSegueWithIdentifier("settingsSegue", sender: self)
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        println("view will appear")
+        loadDefaults()
+        onEditingChanged(tipControl)
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        println("view did appear")
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        println("view will disappear")
+    }
+    
+    override func viewDidDisappear(animated: Bool) {
+        super.viewDidDisappear(animated)
+        println("view did disappear")
+    }
+    
+    func loadDefaults() {
+        var defaults = NSUserDefaults.standardUserDefaults()
+        var low = defaults.integerForKey("lowPercentage")
+        if low != 0 {
+            tipPercentages[0] = low
+        }
+        var medium = defaults.integerForKey("mediumPercentage")
+        if medium != 0 {
+            tipPercentages[1] = medium
+        }
+        var high = defaults.integerForKey("highPercentage")
+        if high != 0 {
+            tipPercentages[2] = high
+        }
+        
+        tipControl.setTitle("\(tipPercentages[0])%", forSegmentAtIndex: 0)
+        tipControl.setTitle("\(tipPercentages[1])%", forSegmentAtIndex: 1)
+        tipControl.setTitle("\(tipPercentages[2])%", forSegmentAtIndex: 2)
+    }
 }
 
